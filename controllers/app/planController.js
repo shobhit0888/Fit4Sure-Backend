6b7cd6b08c52e459c368afcffa50fb510e5553f5
Dhruv-Sharma049 <dhruv75277@gmail.com>
2023-11-19 19:54:19 +0530

    temp edit 1

--- controllers/app/planController.js
+++ controllers/app/planController.js
@@ -5,9 +5,17 @@ class PlanController{
     static selectPlan = async(req,res) => {
         try{
             const user = req.userId
-            
+            const oneDayInMilliseconds = 24 * 60 * 60 * 1000;
+            const duration = req.body.duration;
+            const currdate = Date.now()
+            const trainer = req.body.trainer;
+            const tempDate = new Date(currdate + duration*oneDayInMilliseconds);
+            const endDate = tempDate.toISOString();
             const data = {
-                 plan: req.body.plan
+                 plan: req.body.plan,
+                 duration: req.body.duration,
+                 endDate: endDate,
+                 trainer: trainer
             }
 
             const selectedPlan = await User.findByIdAndUpdate(
--- models/planinfo.js
+++ models/planinfo.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose")
 
 const schema = new mongoose.Schema({
-    plan: {type: mongoose.Schema.Types.ObjectId,
-            ref: "Plan"},
-    status: {type: String, default:"active"}
-})
+  plan: { type: mongoose.Schema.Types.ObjectId, ref: "Plan", unique: true },
+  status: { type: String, default: "active" },
+  trainer: { type: mongoose.Schema.Types.ObjectId, ref: "Trainer" },
+  duration: { type: Number },
+  endDate: { type: Date },
+});
 
 module.exports = mongoose.model("planinfo", schema)
