49eea5057e7d1b8b0988aa3dfcd07c2354e28cbd
Dhruv-Sharma049 <dhruv75277@gmail.com>
2023-11-19 22:59:33 +0530

    temp edit 1

--- routes/admin/dashboard.js
+++ routes/admin/dashboard.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const { NotLoggedIn } = require("../../middlewares/Adminauth");
 const { dashboardData } = require('../../controllers/admin/dashboardController')
 
-router.get("/dashboard", NotLoggedIn, dashboardData);
+router.get("/dashboard",dashboardData);
 
 module.exports = router;
